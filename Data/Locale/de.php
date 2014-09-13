941453e51b669f90cd21094c67968dd533c0eada
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-09-13 05:16:41 +0200

    updated `@link` references

--- Data/Locale/de.php
+++ Data/Locale/de.php
@@ -4,7 +4,7 @@
  * kitFramework::imageTweak
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://kit2.phpmanufaktur.de
+ * @link https://kit2.phpmanufaktur.de/imageTweak
  * @copyright 2014 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  *
